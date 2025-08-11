ed0cdaf643066a6bb5759b3a1c37e90037c88cd1
ReachableCEO <charles@turnsys.com>
2025-08-11 16:11:13 -0500

    merging cleanup rollup. whee!!

---
+++ 2-server/server.js
@@ -0,0 +1,15 @@
+var StaticServer = require('./static-server');
+
+var expressWs = require('express-ws');
+var app = require('express')();
+expressWs(app);
+
+var staticServer = new StaticServer();
+
+app.use('/', staticServer);
+
+var port = process.env.PORT || 8080
+
+app.listen(port, function () {
+    console.log('Open MCT hosted at http://localhost:' + port);
+});
